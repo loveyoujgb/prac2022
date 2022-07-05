761587b7ad672d125e86538e713db38dc5c75440
loveyoujgb <kimmiri235@daum.net>
2022-07-06 00:25:48 +0900

    스스로 한 코드 커밋

--- app.py
+++ app.py
@@ -11,36 +11,60 @@ def home():
 
 # 1. 페이지 로드 시 GET요청
 @app.route("/todo", methods=["GET"])
-def bucket_get():
-  return jsonify({'msg': 'GET 연결 완료!'})
+def todo_get():
+  all_todo_list = list(db.todo.find({}, {'_id': False}))
+  return jsonify({'todo_list': all_todo_list })
 
 # 2. 리스트 추가 시 POST 요청
 @app.route("/todo", methods=["POST"])
 def list_post():
+  all_todo_list = list(db.todo.find({}, {'_id': False}))
+  numbers = len(all_todo_list)+1
+
   list_receive = request.form['list_give']
-  print(list_receive)
+  doc = {'list': list_receive,'number': int(numbers),'done':0}
+  db.todo.insert_one(doc)
   return jsonify({'msg': 'post 연결 완료!'})
 
 # 3. 리스트 완료 시 POST 요청
 @app.route("/todo/done", methods=["POST"])
 def done_post():
   done_receive = request.form['done_give']
-  print(done_receive)
+  db.todo.update_one({'number': int(done_receive)}, {'$set': {'done': 1}})
   return jsonify({'msg': 'todo 완료!'})
+
+# 3-1. 리스트 완료 취소 시 POST 요청
+@app.route("/todo/done_cancel", methods=["POST"])
+def cancel_post():
+  done_receive = request.form['done_give']
+  db.todo.update_one({'number': int(done_receive)}, {'$set': {'done': 0}})
+  return jsonify()
 
 # 4. 리스트 삭제 시 POST 요청
 @app.route("/todo/delete", methods=["POST"])
 def delete_post():
   delete_receive = request.form['delete_give']
-  print(delete_receive)
+  db.todo.delete_one({'number': int(delete_receive)})
   return jsonify({'msg': '삭제 완료!'})
 
-# 5. 리스트 수정 시 POST 요청
+# 5. 리스트 수정 버튼 GET 요청
 @app.route("/todo/edit", methods=["POST"])
-def edit_post():
-  edit_receive = request.form['edit_give']
-  print(edit_receive)
+def edit_get():
+  number_receive = request.form['number_give']
+  print(number_receive)
+  a_list = list(db.todo.find({}, {'_id': False}))
+  print(a_list)
+  return jsonify({'list_number': a_list})
+
+# 6. 리스트 수정탭 안에서 취소시 POST 요청
+@app.route("/todo/success", methods=["POST"])
+def out_post():
+  number_receive = request.form['number_give']
+  change_receive = request.form['change_give']
+  db.todo.update_one({'number': int(number_receive)}, {'$set': {'list': change_receive}})
   return jsonify({'msg': '수정 완료!'})
+
+
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug=True)
--- static/app.js
+++ static/app.js
@@ -9,16 +9,42 @@ $.ajax({
     url: '/todo',
     data: {},
     success: function (response) {
-        alert(response["msg"])
+        const todolist = (response['todo_list'])
+        for (let i=0; i<todolist.length; i++) {
+            let number = todolist[i]['number']
+            let list = todolist[i]['list']
+            let done = todolist[i]['done']
+
+            let temp_html = ``
+            if (done == 0) {
+                temp_html= `<ul id ="edit" class="delete">
+                              <li>${list}</li>                      	
+	                	<button onclick="done_list(${number})">완료</button>
+                        <button onclick="edit_list(${number})">수정</button>
+                        <button onclick="delete_list(${number})">삭제</button>
+                            </ul>`
+            } else {
+                temp_html= `<ul class="delete">
+                              <li style="text-decoration: line-through">${list}</li>
+                              <button onclick="delete_list(${number})">삭제</button>
+                              <button onclick="done_cancel(${number})">완료 취소</button>
+                            </ul>`
+            }
+            $('#todo_List').append(temp_html)
         }
-    })}
+        }
+    });
+}
+
+
 
 // 2. 리스트 추가 시 POST 요청
 function add_list() {
+    const list = $('#list_input').val()
     $.ajax({
         type: 'POST',
         url: '/todo',
-        data: {list_give: '리스트 전송!'},
+        data: {list_give: list},
         success: function (response) {
             alert(response["msg"])
             window.location.reload()
@@ -27,40 +53,87 @@ function add_list() {
 }
 
 // 3. 리스트 완료 시 POST 요청
-function done_list() {
+function done_list(number) {
+    if (confirm("완료 하시겠습니까?") == true){
+                $.ajax({
+            type: 'POST',
+            url: '/todo/done',
+            data: {done_give: number},
+            success: function (response) {
+                window.location.reload()
+            }
+        });
+        alert("완료를 축하합니다!");
+    }else{}
+}
+
+// 3-1. 완료 취소 시 POST 요청
+function done_cancel(number) {
     $.ajax({
         type: 'POST',
-        url: '/todo/done',
-        data: {done_give: '리스트 완료!'},
+        url: '/todo/done_cancel',
+        data: {done_give: number},
         success: function (response) {
-            alert(response["msg"])
             window.location.reload()
         }
-    })
+    });
 }
 
 // 4. 리스트 삭제 시 POST 요청
-function delete_list() {
-    $.ajax({
-        type: 'POST',
-        url: '/todo/delete',
-        data: {delete_give: '리스트 삭제!'},
+function delete_list(number) {
+    if (confirm("취소 하시겠습니까?") == true) {
+        $.ajax({
+            type: 'POST',
+            url: '/todo/delete',
+            data: {delete_give: number},
         success: function (response) {
+                window.location.reload()
+            $("#delete").hide();
             alert(response["msg"])
             window.location.reload()
-        }
-    })
+            }
+        })
+        }else{}
 }
 
-// 5. 리스트 수정 시 POST 요청
-function edit_list() {
+// 5. 리스트 수정 버튼 GET 요청
+function edit_list(number){
     $.ajax({
         type: 'POST',
         url: '/todo/edit',
-        data: {edit_give: '리스트 수정!'},
+        data: {number_give: number},
+        success: function (response) {
+            select_list = (response['list_number'])
+            for(i=0; i<select_list.length; i++){
+                if(select_list[i]['number']==number) {
+                    let the_list = select_list[i]['list']
+                    let temp_html = `<div class=input >
+                                 <input id="edit_input" type="text" placeholder="${the_list}">
+                                 <button onclick="edit_success(${number})">수정완료</button>
+                                 <button onclick="clickout()">취소</button>
+                                 </div>
+                                `
+                    $("#edit").hide();
+                    $('#edit').after(temp_html)
+                } else {}
+            }
+    }})
+}
+
+function clickout() {
+    window.location.reload()
+}
+
+// 6. 리스트 수정 시 POST 요청
+function edit_success(number) {
+    const change_list = $('#edit_input').val()
+    $.ajax({
+        type: 'POST',
+        url: '/todo/success',
+        data: {change_give: change_list,number_give:number},
         success: function (response) {
             alert(response["msg"])
             window.location.reload()
         }
-    })
+    });
 }
